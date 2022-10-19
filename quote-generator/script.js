cb66312fe00e192a03a792a17434cee77ab028ac
olha-leskiv <hello.leskiv@gmail.com>
2022-10-19 22:30:04 +0300

    Added button functionalities

---
+++ quote-generator/script.js
@@ -0,0 +1,54 @@
+// Global Declarations
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+const newQuote = document.getElementById('new-quote');
+const twitterButton = document.getElementById('twitter')
+const quoteContainer = document.querySelector('.quote-text')
+let allQuotes = [];
+
+// Get Quotes from API
+async function initQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const something = await fetch(apiUrl);
+        allQuotes = await something.json();
+    } catch (error) { 
+        alert('Error')
+    }
+}
+
+// Get One Quote 
+async function getOneQuote() {
+    // Pick a random quote from allQuotes array
+    if(allQuotes.length == 0) {
+         await initQuotes();
+    }
+    const oneQuote = allQuotes[Math.floor(Math.random() * allQuotes.length)];
+    
+    // Check if author field is blank and replace it with 'Unknown'
+    if(!oneQuote.author) {
+    author.textContent = "Unkown"; 
+    } else {
+        author.textContent = oneQuote.author; 
+    }
+    
+    // Check if text field very long otherwise change font size
+    quote.textContent = oneQuote.text;
+    if(quote.textContent.length > 120) {
+        quoteContainer.classList.add("long-quote-text")
+    } else {
+        quoteContainer.classList.remove("long-quote-text")
+    }
+}
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote.textContent} - ${author.textContent}`
+    window.open(twitterUrl, '_blank')
+}
+
+getOneQuote()
+
+// Event Listeners
+newQuote.addEventListener('click', getOneQuote)
+twitterButton.addEventListener('click', tweetQuote)
