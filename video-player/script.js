eb44c2d423f2bd9ff03d0d116c5bb50b0ed62d29
olha-leskiv <hello.leskiv@gmail.com>
2022-11-20 20:59:17 +0200

    old solution for volume

--- video-player/script.js
+++ video-player/script.js
@@ -6,13 +6,18 @@ const currentTimeEl = document.getElementById('current-time');
 const progressline = document.getElementById('progressline');
 const timeline = document.getElementById('timeline');
 const duration = document.getElementById('duration');
+const volumeRange = document.getElementById('volume-range');
 const volumeContainer = document.getElementById('volume-container');
 const volumeIcon = document.getElementById('volume-icon');
-const volumeToggle = document.getElementById('volume-toggle');
-const volumelineContainer = document.getElementById('volumeline-container');
-const volumeline = document.getElementById('volumeline');
+// const volumeToggle = document.getElementById('volume-toggle');
+// const volumelineContainer = document.getElementById('volumeline-container');
+// const volumeline = document.getElementById('volumeline');
 
 // volumeline.style.width = video.volume * 100 + "%";
+
+function updateVolumeRange() {
+    volumeRange.value = video.volume * 100;
+}
 
 function videoIsPlaying() {
     return !video.paused
@@ -74,49 +79,84 @@ function formateTime(time) {
 
 function toggleMute() {
     if(video.muted) {
-        volumeIcon.classList.replace('fa-volume-mute', 'fa-volume-high');
         video.muted = false;
+        updateVolumeRange()
     } else {
-        volumeIcon.classList.replace('fa-volume-high', 'fa-volume-mute');
         video.muted = true;
+        volumeRange.value = 0;
+    }
+    changeVolumeIcon();
+}
+
+function changeVolumeIcon() {
+    let volume = video.volume;
+    console.log(volumeRange.value, video.volume)
+
+    if(volume > 0.5) {
+        volumeIcon.className = 'fa-solid fa-volume-high volume';
+    } else if (0.5 > volume && volume > 0) {
+        volumeIcon.className = 'fa-solid fa-volume-low volume';
+    } else if (video.muted) {
+        console.log(video.muted)
+        volumeIcon.className = 'fa-solid fa-volume-mute volume';
     }
 }
 
 function changeVolume(e) {
-    console.log(e)
-    let offsetX = e.clientX - volumelineContainer.getBoundingClientRect().left;
-    let containerWidth = volumelineContainer.offsetWidth;
 
-    if(offsetX < 0) {
-        offsetX = 0;
-    }
-    if(offsetX > containerWidth) {
-        offsetX = containerWidth;
-    }
+    // let offsetX = e.clientX - volumelineContainer.getBoundingClientRect().left;
+    // let containerWidth = volumelineContainer.offsetWidth;
 
-    let volume = offsetX / containerWidth;
+    // if(offsetX < 0) {
+    //     offsetX = 0;
+    // }
+    // if(offsetX > containerWidth) {
+    //     offsetX = containerWidth;
+    // }
 
-    volumeline.style.width = offsetX + "px";
-    volumeToggle.style.left = offsetX + "px";
-    video.volume = volume;
+
+    // volumeline.style.width = offsetX + "px";
+    // volumeToggle.style.left = offsetX + "px";
+    video.volume = e.target.value / 100;
 }
 
-function setVolumeToggle() {
-    let offsetX = video.volume * volumelineContainer.offsetWidth;
-    volumeline.style.width = offsetX + "px";
-    volumeToggle.style.left = offsetX + "px";
-}
+// function changeVolume(e) {
+//     console.log(e)
+//     let offsetX = e.clientX - volumelineContainer.getBoundingClientRect().left;
+//     let containerWidth = volumelineContainer.offsetWidth;
+
+//     if(offsetX < 0) {
+//         offsetX = 0;
+//     }
+//     if(offsetX > containerWidth) {
+//         offsetX = containerWidth;
+//     }
+
+//     let volume = offsetX / containerWidth;
+
+//     volumeline.style.width = offsetX + "px";
+//     volumeToggle.style.left = offsetX + "px";
+//     video.volume = volume;
+// }
+
+// function setVolumeToggle() {
+//     let offsetX = video.volume * volumelineContainer.offsetWidth;
+//     volumeline.style.width = offsetX + "px";
+//     volumeToggle.style.left = offsetX + "px";
+// }
 
 playBtn.addEventListener('click', () => videoIsPlaying() ? pauseVideo() : playVideo());
 expandBtn.addEventListener('click', openFullscreen);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('loadeddata', setDuration);
 video.addEventListener('ended', pauseVideo);
-video.addEventListener('loadeddata', setVolumeToggle)
+volumeRange.addEventListener('input', changeVolumeIcon);
+// video.addEventListener('loadeddata', setVolumeToggle)
 
 volumeIcon.addEventListener('click', toggleMute);
-volumelineContainer.addEventListener('dragover', changeVolume);
+volumeRange.addEventListener('input', changeVolume);
+// volumelineContainer.addEventListener('change', changeVolume);
 
 // volumeToggle.addEventListener('drag', changeVolume)
 
-
+updateVolumeRange();
