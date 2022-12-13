a47e2abf5eb5e23ceb8960714d99eaaa28d3901b
olha-leskiv <hello.leskiv@gmail.com>
2022-12-13 02:55:33 +0200

    sixth version

--- spock-rock-game/script.js
+++ spock-rock-game/script.js
@@ -1,11 +1,12 @@
 const playerIcons = document.querySelectorAll('#player i');
 const playerScoreCont = document.getElementById('player-score');
 const computerScoreCont = document.getElementById('computer-score');
-const computerIcons = document.querySelectorAll('#computer i');
 const playerChoice = document.getElementById('player-choice');
 const computerChoice = document.getElementById('computer-choice');
+const computerIcons = document.querySelectorAll('#computer i');
 const resultText = document.getElementById('result-text');
 const resetBtn = document.getElementById('reset');
+
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
   paper: { name: 'Paper', defeats: ['rock', 'spock'] },
@@ -14,75 +15,77 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
-// let playerScore = 0;
-// let computerScore = 0;
-// let result = "Let's battle!";
-// let computerSelection;
-// let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
+let playerChoiceEl = Element;
+let computerChoiceEl = Element;
 
-// function getComputerSelection() {
-//   computerSelection = choices[Math.floor(Math.random() * 5)];
-// }
-  
-// function select(event, playerInput) {
-//   playerSelection = playerInput;    
-//   getComputerSelection();
+function battle(event) {
+  playerChoiceEl = event.target;
+  computerChoiceEl = computerIcons[Math.floor(Math.random() * 5)];
 
-//   for(let icon of playerIcons) {
-//     icon.classList.remove('selected');
-//   }
-//   event.target.classList.add('selected');
+  if(playerWins()) {
+    playerScore++;
+    setResult('You won!');
+  } else if(computerWins()) {
+    computerScore++;
+    setResult('computer won!');
+  } else {
+    setResult("it's a tie.");
+  }
 
-//   for(let choice in choices) {
-//     let choiceName = choices[choice].name.toLowerCase();
+  updateStyles();
+  showResetBtn();
+}
 
-//     if(choiceName == playerSelection) {
+function playerWins() {
+  let playerType = playerChoiceEl.dataset.type;
+  let computerType = computerChoiceEl.dataset.type;
+  return choices[playerType].defeats.some(type => type === computerType)
+}
 
-//       if(choices[choice].defeats.some((elem) => elem ===   computerSelection)) {
-//         result = 'You won!'         
-//       }
-//     }
+function computerWins() {
+  let playerType = playerChoiceEl.dataset.type;
+  let computerType = computerChoiceEl.dataset.type;
+  return choices[computerType].defeats.some(type => type === playerType)
+}
 
-//     if(choiceName == computerSelection) {
-//       if(choices[choice].defeats.some((elem) => elem === playerSelection)) {
-//         result = 'Computer won!'    
-//       }
-//     } 
-//   }
+function updateStyles() {
+  for(let icon of playerIcons) {
+    icon.classList.remove('selected');
+  }
+  for(let icon of computerIcons) {
+    icon.classList.remove('selected');
+  }
+  playerChoiceEl.classList.add('selected');
+  computerChoiceEl.classList.add('selected');
+  playerScoreCont.textContent = playerScore;
+  computerScoreCont.textContent = computerScore;
+  playerChoice.textContent = " --- " + playerChoiceEl.dataset.type;
+  computerChoice.textContent =  " --- " + computerChoiceEl.dataset.type;
+}
 
-//   updateUI();
-// }
+function setResult(result) {
+  resultText.textContent = result;
+}
 
-// function updateUI() {
-  
-//   for(let icon of computerIcons) {
-//     let type = icon.id.split('-')[1];
+function showResetBtn() {
+  resetBtn.parentElement.classList.add('show');
+}
 
-//     if(type == computerSelection) {
-//       icon.classList.add('selected');
-//       computerChoice.textContent = " --- " + computerSelection;
-//     } else {
-//       icon.classList.add('disabled');
-//       icon.classList.remove('selected');
-//     }
-//   }
+function hideResetBtn() {
+  resetBtn.parentElement.classList.remove('show');
+}
+resetBtn.addEventListener('click', resetGame);
 
-//   resetBtn.parentElement.classList.add('show');
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  hideResetBtn();
+  updateStyles();
+  playerChoice.textContent = '';
+  computerChoice.textContent = '';
+  setResult("Let's battle!");
+}
 
-//   computerScoreCont.textContent = computerScore;
-//   playerScoreCont.textContent = playerScore;
-//   resultText.textContent = result;
-//   console.log(result)
-// }
 
-// resetBtn.addEventListener('click', reset);
-
-// function reset() {
-//   playerScore = 0;
-//   computerScore = 0;
-//   playerSelection = '';
-//   computerSelection = '';
-//   result = "Let's battle!";
-//   resetBtn.parentElement.classList.remove('show');
-//   updateUI();
-// }
