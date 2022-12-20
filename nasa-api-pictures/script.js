6693d34364ba035f4f7f168b9a1df4db32179298
olha-leskiv <hello.leskiv@gmail.com>
2022-12-20 20:21:29 +0200

    nasa api my version finished

--- nasa-api-pictures/script.js
+++ nasa-api-pictures/script.js
@@ -1,33 +1,45 @@
 const apiKey = 'yhXXL435RG7zOb89gTrYfNJcdsoPgKPfHKWJg56F';
-const apiCount = 3;
+const apiCount = 10;
 const apiUrl = `https://api.nasa.gov/planetary/apod?count=${apiCount}&api_key=${apiKey}`;
 
-const loader = document.querySelector('.loader')
+const loader = document.querySelector('.loader');
 const imageContainer = document.querySelector('.images-container');
 const pop = document.querySelector('.save-confirmed');
-const loadMoreNav = document.getElementById('loadMore');
+const loadMoreBtn = document.getElementById('loadMore');
+const emptyState = document.getElementById('emptyState');
 const favouritesNav = document.getElementById('favouritesNav');
+const resultNav = document.getElementById('resultNav');
 
-let favourite = {};
-let images = {};
+let favourite = [];
+let allImages = [];
 
 async function getImageUrlFromApi() {
-    showLoader()
+    display(loader);
     try {
         await fetch(apiUrl)
         .then((response) => response.json())
-        .then((imageCollection) => displayCards(imageCollection));
-        hideLoader();
-        showLoadMore();
+        .then((imageCollection) => processImages(imageCollection));
+        hide(loader);
+        display(loadMoreBtn);
     } catch (error) {
         throw error
     }
 }
 
-function displayCards(collection) {
-    for(image of collection) {
+function processImages(collection) {
+    addToImages(collection);
+    displayCardsFrom(collection);
+}
+
+function addToImages(images) {
+    for(let image of images) {
+        allImages.push(image);
+    };
+}
+
+function displayCardsFrom(images) {
+    for(let image of images) {
         createCard(image);
-        images[image.title] = image; 
     }
 }
 
@@ -39,87 +51,154 @@ function createCard(image) {
     a.setAttribute('target',"_blank");
     a.setAttribute('href',image.hdurl);
     a.setAttribute('title',"View Full Image");
-
     
     let topImage = document.createElement('img');
-    // topImage.addEventListener('load', hideLoader);
     topImage.src = image.url;
     topImage.setAttribute('alt', image.title);
     topImage.className = 'card-img-top';
     topImage.loading = 'lazy';
     
-    a.append(topImage);
-
-    card.append(a);
-
     let cardBody = document.createElement('div');
     cardBody.className = 'card-body';
     cardBody.innerHTML = `
-    <p class="card-text">${image.explanation}</p>
-    <small class="text-muted">
-        <strong>${image.date}</strong>
-        <span>Copyright Info</span>
-    </small>`
+        <p class="card-text">${image.explanation}</p>
+        <small class="text-muted">
+            <strong>${image.date}</strong>
+            <span>Copyright Info</span>
+        </small>
+    `
 
     let favouriteBtn = document.createElement('div');
     favouriteBtn.className = 'card-favourite clickable'
-    favouriteBtn.innerHTML = `<i class="fa-regular fa-bookmark"></i><p>Add to Favourites</p>`;
-
+    if(image.favourite) {
+        favouriteBtn.innerHTML = `
+        <i class="fa-solid fa-heart"></i><p>Remove from Favourites</p>`; 
+    } else {
+        favouriteBtn.innerHTML = `
+        <i class="fa-regular fa-heart"></i><p>Add to Favourites</p>`;
+    }
     favouriteBtn.addEventListener('click', (e) => {
-        let icon = e.currentTarget.firstElementChild;
-        let text = e.currentTarget.lastElementChild
-        if(favourite[image.title]) {
-            delete favourite[image.title];
-            icon.classList.replace('fa-solid', 'fa-regular');
-            text.textContent = 'Add to Favourites';
-        } else {
-            favourite[image.title] = image;
-            icon.classList.replace('fa-regular', 'fa-solid');
-            text.innerHTML = 'Added to your Favourites';
-            pop.classList.remove('hidden');
-            setTimeout(() => {
-                pop.classList.add('hidden')
-            }, 3000)
-
+        let item = {
+            icon: e.currentTarget.firstElementChild,
+            text: e.currentTarget.lastElementChild,
+            card: e.currentTarget.closest('.card'),
+            image: image,
         }
-        console.log(favourite)
+        toggleFavourite(item);
+        updateLocalStorage();
     });
-
-    cardBody.prepend(favouriteBtn);
     
     let title = document.createElement('h5');
     title.className = 'card-title';
     title.textContent = image.title;
-
+    
+    a.append(topImage);
+    cardBody.prepend(favouriteBtn);
     cardBody.prepend(title);
-
+    card.append(a);
     card.append(cardBody);
-
     imageContainer.append(card);
 }
 
-function showLoader() {
-    loader.classList.remove('hidden');
-}
+function toggleFavourite(item) {
+    if(itemExist()) {
+        removeFromFavourites();
+        if(favourite.length == 0 && isFavouritesPage()) {
+            display(emptyState);
+        }
+    } else {
+        addToFavourites();
+    }
 
-function hideLoader() {
-    loader.classList.add('hidden');
-}
+    function itemExist() {
+        return favourite.indexOf(item.image) > -1
+    }
 
-function showLoadMore() {
-    loadMoreNav.classList.remove('hidden')
-}
+    function removeFromFavourites() {
+        let index = favourite.indexOf[item.image];
+        favourite.splice(index, 1);
+        item['icon'].classList.replace('fa-solid', 'fa-regular');
+        item['text'].textContent = 'Add to Favourites';
+        item['image'].favourite = false;
+        if(isFavouritesPage()) {
+            item['card'].remove();
+        }
+    }
 
-loadMoreNav.addEventListener('click', getImageUrlFromApi);
-favouritesNav.addEventListener('click', loadFavourites)
+    function isFavouritesPage() {
+        return favouritesNav.classList.contains('active');
+    }
+
+    function addToFavourites() {
+        favourite.push(item.image);
+        item['icon'].classList.replace('fa-regular', 'fa-solid');
+        item['text'].innerHTML = 'Remove from Favourites';
+        display(pop);
+        item['image'].favourite = true;
+        setTimeout(() => {
+            hide(pop);
+        }, 3000)
+    }
+}
 
 function loadFavourites() {
+    removeCards();
+    hide(loadMoreBtn);
+    updateNavState();
+    if(!favourite.length) { 
+        display(emptyState);
+        return;
+    }
+    scrollTo({top: 0, behavior: 'smooth'});
+    displayCardsFrom(favourite);
+}
+
+function loadMain() {
+    removeCards();
+    displayCardsFrom(allImages);
+    hide(emptyState);
+    display(loadMoreBtn);
+    updateNavState();
+}
+
+function removeCards() {
     let allCards = document.querySelectorAll('.card');
     for(let card of allCards) {
         card.remove();
     }
-    displayCards(favourite)
 }
 
+function hide(element) {
+    element.classList.add('hidden');
+}
+
+function display(element) {
+    element.classList.remove('hidden');
+}
+
+function updateNavState() {
+    resultNav.classList.toggle('active');
+    favouritesNav.classList.toggle('active');
+}
+
+function initStateFromLocalStorage() {
+    if(localStorage['favourite']) {
+        favourite = JSON.parse(localStorage.getItem('favourite'));
+    }
+}
+
+function updateLocalStorage() {
+    localStorage.setItem(
+        'favourite',
+        JSON.stringify(favourite)
+    )
+}
+
+loadMoreBtn.addEventListener('click', getImageUrlFromApi);
+favouritesNav.addEventListener('click', loadFavourites);
+resultNav.addEventListener('click', loadMain);
+emptyState.lastElementChild.addEventListener('click', loadMain);
+
 getImageUrlFromApi()
+initStateFromLocalStorage();
 
