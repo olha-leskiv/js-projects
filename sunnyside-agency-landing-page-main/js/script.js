9827fa89f2776d30e46264bf1661c18f0be34099
olha-leskiv <hello.leskiv@gmail.com>
2023-04-18 18:46:13 +0300

    sunnyside finished

---
+++ sunnyside-agency-landing-page-main/js/script.js
@@ -0,0 +1,19 @@
+let mobileNav = document.getElementById('mobileNav');
+burgerIcon = document.getElementById('burgerIcon');
+
+burgerIcon.onclick = () => {
+    mobileNav.classList.toggle('nav-mobile');
+}
+
+window.onresize = () => {
+    if(window.visualViewport.width > 850) {
+        mobileNav.classList.remove('nav-mobile');
+    }
+}
+
+window.onload = () => {
+    if(window.visualViewport.width > 850) {
+        mobileNav.classList.remove('nav-mobile');
+    }
+}
+
